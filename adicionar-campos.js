b32e85d4767315361ab56211194e09dc10119962
BrunaFPereira <brunafpereira15@gmail.com>
2022-10-31 15:33:43 -0300

    desafio3 adicionando campos

---
+++ adicionar-campos.js
@@ -0,0 +1,12 @@
+const cliente = {
+    nome: "Andre",
+    idade: 36,
+    cpf: "02983747565",
+    email: "andrezim@email.com"
+}
+
+console.log(cliente);
+cliente.fone="987345677"
+console.log(cliente);
+cliente.fone="(12)987345677"
+console.log(cliente)
